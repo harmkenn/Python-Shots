6cb5bb0f00521ee12021911a171b30330d8900d9
Ken Harmon <harmkenn@gmail.com>
2022-04-09 20:56:53 +0000

    Year

--- apps/f_sungrid.py
+++ apps/f_sungrid.py
@@ -1,22 +1,20 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 def app():
     # title of the app
     st.markdown('Get your position from the location of the sun')
     c1,c2 = st.columns((1,3))
     with c1:
         now_utc = datetime.now(timezone.utc) # UTC time
-        rightnow = datetime.now()
+        rightnow = datetime.now(timezone.utc)
         dt = rightnow.astimezone() # local time
         year = rightnow.year
-        if st.button('Now'):
-            rightnow = datetime.now()
-            year = rightnow.year
-        year = st.number_input('Year: ',1900,2100,year)
+        yeard = st.number_input('Year: ',1900,2100,year)
+        setday = rightnow - timedelta(days=365*(year - yeard))
     with c2:
-        st.write(year)
+        st.write(str(setday)+'UTC')
         
 
     

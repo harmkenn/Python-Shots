2da9fa3f2efa79f89d56dd89ad6a4175a2b116c7
Ken Harmon <harmkenn@gmail.com>
2022-04-09 21:53:20 +0000

    Good set

--- apps/f_sungrid.py
+++ apps/f_sungrid.py
@@ -7,14 +7,24 @@ def app():
     st.markdown('Get your position from the location of the sun')
     c1,c2 = st.columns((1,3))
     with c1:
-        now_utc = datetime.now(timezone.utc) # UTC time
-        rightnow = datetime.now(timezone.utc)
-        dt = rightnow.astimezone() # local time
-        year = rightnow.year
-        yeard = st.number_input('Year: ',1900,2100,year)
-        setday = rightnow - timedelta(days=365*(year - yeard))
+        rightnow = datetime.now(timezone.utc) #Pull current zulu time
+        setyear = st.number_input('Year: ',1900,2100,rightnow.year)
+        setmonth = st.number_input('Month: ',1,12,rightnow.month)
+        setday = st.number_input('Day: ',1,31,rightnow.day)
+        sethour = st.number_input('Hour: ',0,23,rightnow.hour)
+        setmin = st.number_input('Minute: ',0,59,rightnow.minute)
+        setsec = st.number_input('Second: ',0,59,rightnow.second)
+        
+        setday = datetime(setyear,setmonth,setday,sethour,setmin,setsec)
+
+        
+
     with c2:
+        if st.button('Now'):
+            setday = datetime.now(timezone.utc)
         st.write(str(setday)+'UTC')
+
+        
         
 
     

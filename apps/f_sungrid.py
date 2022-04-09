6bee131e88827abc2ac368285b7cf422fd8b636a
Ken Harmon <harmkenn@gmail.com>
2022-04-09 23:28:02 +0000

    Sub solar point

--- apps/f_sungrid.py
+++ apps/f_sungrid.py
@@ -7,13 +7,13 @@ def app():
     st.markdown('Get your position from the location of the sun')
     c1,c2 = st.columns((1,3))
     with c1:
-        rightnow = datetime.now(timezone.utc) #Pull current zulu time
-        setyear = st.number_input('Year: ',1900,2100,rightnow.year)
-        setmonth = st.number_input('Month: ',1,12,rightnow.month)
-        setday = st.number_input('Day: ',1,31,rightnow.day)
-        sethour = st.number_input('Hour: ',0,23,rightnow.hour)
-        setmin = st.number_input('Minute: ',0,59,rightnow.minute)
-        setsec = st.number_input('Second: ',0,59,rightnow.second)
+        
+        setyear = st.number_input('Year: ',1900,2100,2022)
+        setmonth = st.number_input('Month: ',1,12,4)
+        setday = st.number_input('Day: ',1,31,9)
+        sethour = st.number_input('Hour: ',0,23,22)
+        setmin = st.number_input('Minute: ',0,59,40)
+        setsec = st.number_input('Second: ',0,59,0)
         
         setday = datetime(setyear,setmonth,setday,sethour,setmin,setsec)
 
@@ -23,6 +23,15 @@ def app():
         if st.button('Now'):
             setday = datetime.now(timezone.utc)
         st.write(str(setday)+'UTC')
+        sslon = 180-(setday.hour+setday.minute/60+setday.second/3600)*15+22/60
+        st.write(sslon)
+        julian = int(setday.strftime('%j')) + (setday.hour+setday.minute/60+setday.second/3600)/24
+        a = 360/365.24*(julian - 2)*np.pi/180
+        b = (360/365.24*(julian+10)+360/np.pi*.0167*np.sin(a))*np.pi/180
+        c = np.sin(-23.44*np.pi/180)*np.cos(b)
+        sslat = np.arcsin(c)*180/np.pi - .07
+        st.write(julian)
+        st.write(sslat)
 
         
         

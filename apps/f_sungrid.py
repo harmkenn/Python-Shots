e86514f03b584f934d39057f743d252d35a0dcf9
Ken Harmon <harmkenn@isu.edu>
2022-12-13 11:41:24 +0000

    More MGRS

--- apps/f_sungrid.py
+++ apps/f_sungrid.py
@@ -34,7 +34,7 @@ def app():
         b = (360/365.24*(julian+10)+360/np.pi*.0167*np.sin(a))*np.pi/180
         c = np.sin(-23.44*np.pi/180)*np.cos(b)
         sslat = np.arcsin(c)*180/np.pi - .07
-        st.write('Sub Solar Point: '+str(sslat)+', '+str(sslon),', MGRS: '+ zf.LL2MGRS(sslat,sslon)[0])
+        st.write('Sub Solar Point: '+str(sslat)+', '+str(sslon),', MGRS: '+ zf.LL2MGRS(sslat,sslon)[1])
 
         azsun = st.number_input('Azimuth to the Sun',0.00,360.00,10.0)
         bazsun = azsun + 180
